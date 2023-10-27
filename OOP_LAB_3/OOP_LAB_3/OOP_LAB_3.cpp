e5e04a57c6d9e915996f3edbd0efd42ca2f1598e
B0kutachi <145747409+B0kutachi@users.noreply.github.com>
2023-10-27 19:50:50 +0700

    Add files via upload

---
+++ OOP_LAB_3/OOP_LAB_3/OOP_LAB_3.cpp
@@ -0,0 +1,69 @@
+﻿#include <iostream>
+#include <string>
+
+#include "Book.h"
+#include "Route.h"
+#include "Point_Rectangle.h"
+#include "Time_Flight.h"
+#include "Music.h"
+
+using namespace std;
+
+void Breakpoints1()
+{
+	DemoBook();
+}
+
+void Breakpoints2()
+{
+	DemoRoute();
+}
+
+void Breakpoints3()
+{
+	DemoRectangleWithPoint();
+}
+
+void Breakpoints4()
+{
+	DemoFlightWithTime();
+	
+	cout << endl << endl;
+	Flight flight;
+	FlightBuild(flight);
+}
+
+void Breakpoints5()
+{
+	DemoBand();
+}
+
+int main()
+{
+	int x = 0;
+	cout << "\nEnter the case: ";
+	cin >> x;
+	switch (x) {
+	case 1:
+		Breakpoints1();   // Tasks #3.2.1 - 3.2.5
+		break;
+	case 2:
+		Breakpoints2();   // Task #3.2.6
+		break;
+	case 3:
+		Breakpoints3();   // Tasks #3.3.1 - 3.3.5
+		break;
+	case 4:
+		Breakpoints4();   // Tasks 3.3.7 - 3.3.11
+		break;
+	case 5:
+		Breakpoints5();   // Tasks 3.4.1 - 3.4.6
+		break;
+	default:
+		cout << "Invalid value. Try again.";
+	}
+
+	cout << endl << endl;
+	return 0;
+}
+
